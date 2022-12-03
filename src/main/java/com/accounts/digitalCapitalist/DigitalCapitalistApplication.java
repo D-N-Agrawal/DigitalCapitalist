66cdf271812a0980f2162bb75cf8a59cb9db3bd2
D-N-Agrawal <anshdev1712@outlook.com>
2022-12-03 19:42:29 +0530

    first commit

---
+++ src/main/java/com/accounts/digitalCapitalist/DigitalCapitalistApplication.java
@@ -0,0 +1,13 @@
+package com.accounts.digitalCapitalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalCapitalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalCapitalistApplication.class, args);
+	}
+
+}
